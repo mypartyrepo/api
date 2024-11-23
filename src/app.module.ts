da8ca1f8273bd65bae76ae2f60193434886c1f3a
oleosb <oleosbo@gmail.com>
2024-11-22 21:15:42 -0300

    user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 import config from './config/config';
 
 @Module({
@@ -30,6 +31,7 @@ import config from './config/config';
       inject: [ConfigService],
     }),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
